0111fb099cfae340ec747e4cb28b3841c41fc45f
Diana Melendez Z <DianaMelendezZ@hotmail.com>
2017-07-17 17:27:29 -0500

    Lee los datos

--- Planetas.c
+++ Planetas.c
@@ -1,6 +1,31 @@
 // Librerias
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Leer datos archivo
+int main()
+{
+	FILE *coordenadas;
+	coordenadas = fopen("coordinates(2).csv", "r");
 
+	const char *delimiter;
+	delimiter = ",";
+
+	int l = 500;
+	char linea_temp[l];
+
+	char *div;
+
+	while(fgets(linea_temp, l, coordenadas))
+	{
+		printf("Fila: %s", linea_temp);
+		div = strtok(linea_temp, delimiter);
+	}
+}
+
+int almacena()
+{
+	
+}
